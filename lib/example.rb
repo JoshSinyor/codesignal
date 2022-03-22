8d0362d2d44851a9256c95a19657116b0de9cde6
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-22 11:16:31 +0000

    Update example.rb and example_spec.rb. Reflects completion of adjacentElementsProduct.

--- lib/example.rb
+++ lib/example.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-def example_method; end
+def example_method(inputArray)
+  outputArray = []
+
+  inputArray.each_with_index do |element, index|
+    break if (index + 1) == inputArray.length
+    outputArray.push(element * inputArray[index + 1])
+  end
+
+  outputArray.sort[-1]
+end
--- spec/units/example_spec.rb
+++ spec/units/example_spec.rb
@@ -3,4 +3,7 @@
 require 'example'
 
 describe 'example_method' do
+  it 'passes the first test' do
+    expect(example_method([3, 6, -2, -5, 7, 3])).to eq 21
+  end
 end
