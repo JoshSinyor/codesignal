5321a3ecd2ab85dc266d7a17ad483b2679ab615b
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-22 11:49:34 +0000

    Update example.rb and example_spec.rb. Reflects completion of shapeArea.

--- lib/example.rb
+++ lib/example.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-def example_method(inputArray)
-  outputArray = []
-
-  inputArray.each_with_index do |element, index|
-    break if (index + 1) == inputArray.length
-    outputArray.push(element * inputArray[index + 1])
-  end
-
-  outputArray.sort[-1]
+def example_method(n)
+  (n ** 2) + ((n - 1) ** 2)
 end
