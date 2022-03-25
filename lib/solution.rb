7def989a0ed6d15321dc2a1d5e704755f9c4690b
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-25 13:43:53 +0000

    Update solution.rb and solution_spec.rb. Reflects completion of arrayChange.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,36 +1,16 @@
 # frozen_string_literal: true
 
-def solution(first_array, second_array)
-  first_array.sort == second_array.sort and first_array.each_with_index.count do |instance, index|
-    second_array[index] != instance
-  end < 3
+def solution(input_array)
+  moves = 0
+
+  input_array[1..].each_index do |index|
+    next if input_array[index + 1] > input_array[index]
+
+    move = (input_array[index] - input_array[index + 1]) + 1
+
+    input_array[index + 1] += move
+    moves += move
+  end
+
+  moves
 end
-
-# Alternative solution
-
-# def solution(first_array, second_array)
-#   return true if first_array == second_array
-#   return false if first_array.sort != second_array.sort
-#
-#   parse_array(first_array, second_array)
-# end
-#
-# private
-#
-# def parse_array(first_array, second_array)
-#   second_array.each_with_index do |element, index|
-#     next if element == first_array[index]
-#
-#     instances = second_array.each_index.select { |instance| second_array[instance] == first_array[index] }
-#
-#     instances.each do |instance_index|
-#       third_array = second_array.dup
-#       third_array[index] = first_array[index]
-#       third_array[instance_index] = element
-#
-#       return true if first_array == third_array
-#     end
-#
-#     return false
-#   end
-# end
