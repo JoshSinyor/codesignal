4558e9a501e7d88815bde602e6f0a628645046fc
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-04 17:56:17 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of isIPv4Address.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
-def solution(input_array)
-  input_array.each_cons(2).to_a.map { |consecutive_pair| consecutive_pair.reduce(:-).abs }.max
+# require 'ipaddr' Required by CodeSignal
+
+def solution(input_string)
+  IPAddr.new(input_string).ipv4?
+rescue ArgumentError
+  false
 end
 
-# Alternative solution using index
+# Alternate solution using only regex
 
-# def solution(input_array)
-#   differences = []
-#   input_array[...-1].each_with_index { |element, index| differences.push((element - input_array[index + 1]).abs) }
-#   differences.max
+# def solution(input_string)
+#   input_string =~ /^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$/ ? true : false
+# end
+
+# Alternate solution with more legible regex
+
+# def solution(input_string)
+#   return false unless input_string =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/
+#
+#   input_string = input_string.split('.')
+#
+#   input_string.each do |range|
+#     return false if range != range.to_i.to_s
+#     return false if range.to_i.negative? || range.to_i > 255
+#   end
+#
+#   true
 # end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,13 +3,27 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input: [2, 4, 1, 0], output: 3 }, # 01
-  { test: 2, input: [1, 1, 1, 1], output: 0 }, # 02
-  { test: 3, input: [-1, 4, 10, 3, -2], output: 7 }, # 03
-  { test: 4, input: [10, 11, 13], output: 2 }, # 04
-  { test: 5, input: [-2, -2, -2, -2, -2], output: 0 }, # 05
-  { test: 6, input: [-1, 1, -3, -4], output: 4 }, # 06
-  { test: 7, input: [-14, 15, -15], output: 30 } # 07
+  { test: 1, input: '172.16.254.1', output: true }, # 01
+  { test: 2, input: '172.316.254.1', output: false }, # 02
+  { test: 3, input: '.254.255.0', output: false }, # 03
+  { test: 4, input: '1.1.1.1a', output: false }, # 04
+  { test: 5, input: '1', output: false }, # 05
+  { test: 6, input: '0.254.255.0', output: true }, # 06
+  { test: 7, input: '1.23.256.255.', output: false }, # 07
+  { test: 8, input: '1.23.256..', output: false }, # 08
+  { test: 9, input: '0..1.0', output: false }, # 09
+  { test: 10, input: '64.233.161.00', output: false }, # 10
+  { test: 11, input: '64.00.161.131', output: false }, # 11
+  { test: 12, input: '01.233.161.131', output: false }, # 12
+  { test: 13, input: '35..36.9.9.0', output: false }, # 13
+  { test: 14, input: '1.1.1.1.1', output: false }, # 14
+  { test: 15, input: '1.256.1.1', output: false }, # 15
+  { test: 16, input: 'a0.1.1.1', output: false }, # 16
+  { test: 17, input: '0.1.1.256', output: false }, # 17
+  { test: 18, input: '129380129831213981.255.255.255', output: false }, # 18
+  { test: 19, input: '255.255.255.255abcdekjhf', output: false }, # 19
+  { test: 20, input: '7283728', output: false }, # 20
+  { test: 21, input: '0..1.0.0', output: false }  # 21
 ].freeze
 
 describe 'solution' do
