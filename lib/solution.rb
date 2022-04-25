773fe6edd4888c4f6fb0b6919b157d80184648a7
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-25 18:40:18 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of depositProfit.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
-def solution(points, first_number)
-  (first_number + points / 2) % points
+def solution(deposit, rate, threshold)
+  Math.log((threshold.to_f / deposit), 1 + (rate.to_f / 100)).ceil
 end
+
+# Alternative, insufficiently fast iterative solution
+
+# def solution(deposit, rate, threshold)
+#   i = 0
+
+#   until deposit >= threshold
+#     deposit += deposit * rate / 100
+#     i += 1
+#   end
+
+#   i
+# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,20 +3,18 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input1: 10, input2: 2, output: 7 }, # 01
-  { test: 2, input1: 10, input2: 7, output: 2 }, # 02
-  { test: 3, input1: 4, input2: 1, output: 3 }, # 03
-  { test: 4, input1: 6, input2: 3, output: 0 }, # 04
-  { test: 5, input1: 18, input2: 6, output: 15 }, # 05
-  { test: 6, input1: 12, input2: 10, output: 4 }, # 06
-  { test: 7, input1: 18, input2: 5, output: 14 } # 07
+  { test: 1, input1: 100, input2: 20, input3: 170, output: 3 }, # 01
+  { test: 2, input1: 100, input2: 1, input3: 101, output: 1 }, # 02
+  { test: 3, input1: 1, input2: 100, input3: 64, output: 6 }, # 03
+  { test: 4, input1: 20, input2: 20, input3: 50, output: 6 }, # 04
+  { test: 5, input1: 50, input2: 25, input3: 100, output: 4 } # 05
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 2 inputs and 1 output' do
+  it 'passes the supplied tests with 3 inputs and 1 output' do
     TEST_I_O.each do |element|
       puts "Executing test #{element[:test]}..."
-      expect(solution(element[:input1], element[:input2])).to eq(element[:output])
+      expect(solution(element[:input1], element[:input2], element[:input3])).to eq(element[:output])
       puts 'Passed.'
     end
   end
