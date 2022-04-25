64c8d40bb5867d52cd14ae938c032cfb294b95ab
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-25 18:01:54 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Circle of Numbers.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,28 +1,5 @@
 # frozen_string_literal: true
 
-def solution(cell1, cell2)
-  (cell1[0].ord + cell1[1].to_i).odd? == (cell2[0].ord + cell2[1].to_i).odd?
+def solution(points, first_number)
+  (first_number + points / 2) % points
 end
-
-# Alternate solution using match
-
-# def solution(cell1, cell2)
-#   cells = [cell1, cell2]
-
-#   cells.map! do |cell|
-#     case cell.chars[0].match?(/[ACEG]/)
-#     when true
-#       cell.chars[1].to_i.even? ? 'white' : 'black'
-#     when false
-#       cell.chars[1].to_i.odd? ? 'white' : 'black'
-#     end
-#   end
-
-#   cells[0] == cells[1]
-# end
-
-# Alternate solution using codepoints instead of ord
-
-# def solution(cell1, cell2)
-#   (cell1[0].codepoints[0] + cell1[1].to_i).even? == (cell2[0].codepoints[0] + cell2[1].to_i).even?
-# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,16 +3,13 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input1: 'A1', input2: 'C3', output: true }, # 01
-  { test: 2, input1: 'A1', input2: 'H3', output: false }, # 02
-  { test: 3, input1: 'A1', input2: 'A2', output: false }, # 03
-  { test: 4, input1: 'A1', input2: 'B2', output: true }, # 04
-  { test: 5, input1: 'B3', input2: 'H8', output: false }, # 05
-  { test: 6, input1: 'C3', input2: 'B5', output: false }, # 06
-  { test: 7, input1: 'G5', input2: 'E7', output: true }, # 07
-  { test: 8, input1: 'C8', input2: 'H8', output: false }, # 08
-  { test: 9, input1: 'D2', input2: 'D2', output: true }, # 09
-  { test: 10, input1: 'A2', input2: 'A5', output: false } # 10
+  { test: 1, input1: 10, input2: 2, output: 7 }, # 01
+  { test: 2, input1: 10, input2: 7, output: 2 }, # 02
+  { test: 3, input1: 4, input2: 1, output: 3 }, # 03
+  { test: 4, input1: 6, input2: 3, output: 0 }, # 04
+  { test: 5, input1: 18, input2: 6, output: 15 }, # 05
+  { test: 6, input1: 12, input2: 10, output: 4 }, # 06
+  { test: 7, input1: 18, input2: 5, output: 14 } # 07
 ].freeze
 
 describe 'solution' do
