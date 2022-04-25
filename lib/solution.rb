cbb3dbd2b26873bb3f884ce3e661ea41703953ac
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-25 16:54:16 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of alphabeticShift.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-def solution(variable_name)
-  variable_name.match?(/^[^\W\d]\w*+$/)
+def solution(input_string)
+  input_string.tr('a-yz', 'b-za')
 end
+
+# Alternative solution using next method
+
+# def solution(input_string)
+#   input_string.chars.map { |letter| letter == 'z' ? 'a' : letter.next }.join
+# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,16 +3,11 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input: 'var_1__Int', output: true }, # 01
-  { test: 2, input: 'qq-q', output: false }, # 02
-  { test: 3, input: '2w2', output: false }, # 03
-  { test: 4, input: ' variable', output: false }, # 04
-  { test: 5, input: 'va[riable0', output: false }, # 05
-  { test: 6, input: 'variable0', output: true }, # 06
-  { test: 7, input: 'a', output: true }, # 07
-  { test: 8, input: '_Aas_23', output: true }, # 08
-  { test: 9, input: 'a a_2', output: false }, # 09
-  { test: 10, input: '0ss', output: false } # 10
+  { test: 1, input: 'crazy', output: 'dsbaz' }, # 01
+  { test: 2, input: 'z', output: 'a' }, # 02
+  { test: 3, input: 'aaaabbbccd', output: 'bbbbcccdde' }, # 03
+  { test: 4, input: 'fuzzy', output: 'gvaaz' }, # 04
+  { test: 5, input: 'codesignal', output: 'dpeftjhobm' } # 05
 ].freeze
 
 describe 'solution' do
