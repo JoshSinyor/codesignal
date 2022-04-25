b7f084fa28ab9c62ed2d1e4c7007cf9468500f34
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-25 16:09:56 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Array Replace.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,25 +1,5 @@
 # frozen_string_literal: true
 
-def solution(input_array)
-  output_array = []
-
-  border = [false] * (input_array.first.length + 2)
-  input_array = [border] + input_array.map { |row| [false] + row + [false] } + [border]
-
-  input_array[1..-2].each_with_index do |row, row_index|
-    interim_array = []
-
-    row[1..-2].each_index do |col_index|
-      first_row = input_array[row_index][col_index, 3]
-      second_row = input_array[row_index + 1][col_index, 3]
-      second_row.delete_at(1)
-      third_row = input_array[row_index + 2][col_index, 3]
-
-      interim_array.push((first_row + second_row + third_row).count(true))
-    end
-
-    output_array.push(interim_array)
-  end
-
-  output_array
+def solution(input_array, old_element, new_element)
+  input_array.map { |element| element == old_element ? new_element : element }
 end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,19 +3,21 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input: [[true, false, false], [false, true, false], [false, false, false]], output: [[1, 2, 1], [2, 1, 1], [1, 1, 1]] }, # 01
-  { test: 2, input: [[false, false, false], [false, false, false]], output: [[0, 0, 0], [0, 0, 0]] }, # 02
-  { test: 3, input: [[true, false, false, true], [false, false, true, false], [true, true, false, true]], output: [[0, 2, 2, 1], [3, 4, 3, 3], [1, 2, 3, 1]] }, # 03
-  { test: 4, input: [[true, true, true], [true, true, true], [true, true, true]], output: [[3, 5, 3], [5, 8, 5], [3, 5, 3]] }, # 04
-  { test: 5, input: [[false, true, true, false], [true, true, false, true], [false, false, true, false]], output: [[3, 3, 3, 2], [2, 4, 5, 2], [2, 3, 2, 2]] }, # 05
-  { test: 6, input: [[true, false], [true, false], [false, true], [false, false], [false, false]], output: [[1, 2], [2, 3], [2, 1], [1, 1], [0, 0]] } # 06
+  { test: 1, input1: [1, 2, 1], input2: 1, input3: 3, output: [3, 2, 3] }, # 01
+  { test: 2, input1: [1, 2, 3, 4, 5], input2: 3, input3: 0, output: [1, 2, 0, 4, 5] }, # 02
+  { test: 3, input1: [1, 1, 1], input2: 1, input3: 10, output: [10, 10, 10] }, # 03
+  { test: 4, input1: [1, 2, 1, 2, 1], input2: 2, input3: 1, output: [1, 1, 1, 1, 1] }, # 04
+  { test: 5, input1: [1, 2, 1, 2, 1], input2: 2, input3: 2, output: [1, 2, 1, 2, 1] }, # 05
+  { test: 6, input1: [3, 1], input2: 3, input3: 9, output: [9, 1] }, # 06
+  { test: 7, input1: [10, 10], input2: 0, input3: 9, output: [10, 10] }, # 07
+  { test: 8, input1: [2, 1], input2: 3, input3: 9, output: [2, 1] } # 08
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 1 input and 1 output' do
+  it 'passes the supplied tests with 3 inputs and 1 output' do
     TEST_I_O.each do |element|
       puts "Executing test #{element[:test]}..."
-      expect(solution(element[:input])).to eq(element[:output])
+      expect(solution(element[:input1], element[:input2], element[:input3])).to eq(element[:output])
       puts 'Passed.'
     end
   end
