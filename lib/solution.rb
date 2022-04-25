e1819733cb7e04b17bbff8d589cad717720d847d
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-25 16:19:43 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of evenDigitsOnly.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-def solution(input_array, old_element, new_element)
-  input_array.map { |element| element == old_element ? new_element : element }
+def solution(input_number)
+  input_number.digits.all?(&:even?)
 end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,21 +3,23 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input1: [1, 2, 1], input2: 1, input3: 3, output: [3, 2, 3] }, # 01
-  { test: 2, input1: [1, 2, 3, 4, 5], input2: 3, input3: 0, output: [1, 2, 0, 4, 5] }, # 02
-  { test: 3, input1: [1, 1, 1], input2: 1, input3: 10, output: [10, 10, 10] }, # 03
-  { test: 4, input1: [1, 2, 1, 2, 1], input2: 2, input3: 1, output: [1, 1, 1, 1, 1] }, # 04
-  { test: 5, input1: [1, 2, 1, 2, 1], input2: 2, input3: 2, output: [1, 2, 1, 2, 1] }, # 05
-  { test: 6, input1: [3, 1], input2: 3, input3: 9, output: [9, 1] }, # 06
-  { test: 7, input1: [10, 10], input2: 0, input3: 9, output: [10, 10] }, # 07
-  { test: 8, input1: [2, 1], input2: 3, input3: 9, output: [2, 1] } # 08
+  { test: 1, input: 248_622, output: true }, # 01
+  { test: 2, input: 642_386, output: false }, # 02
+  { test: 3, input: 248_842, output: true }, # 03
+  { test: 4, input: 1, output: false }, # 04
+  { test: 5, input: 8, output: true }, # 05
+  { test: 6, input: 2_462_487, output: false }, # 06
+  { test: 7, input: 468_402_800, output: true }, # 07
+  { test: 8, input: 2_468_428, output: true }, # 08
+  { test: 9, input: 5_468_428, output: false }, # 09
+  { test: 10, input: 7_468_428, output: false } # 10
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 3 inputs and 1 output' do
+  it 'passes the supplied tests with 1 input and 1 output' do
     TEST_I_O.each do |element|
       puts "Executing test #{element[:test]}..."
-      expect(solution(element[:input1], element[:input2], element[:input3])).to eq(element[:output])
+      expect(solution(element[:input])).to eq(element[:output])
       puts 'Passed.'
     end
   end
