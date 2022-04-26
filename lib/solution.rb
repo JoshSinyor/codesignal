ade28b2d243f261f0e0648cc68a2b1dfdd36f92c
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-26 19:08:31 +0100

    Add docs/*. Update solution.rb and solution_spec.rb. Reflects completion of arrayMaxConsecutiveSum.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,5 +1,47 @@
 # frozen_string_literal: true
 
-def solution(input_string)
-  input_string.chars.uniq.size
+def solution(input_array, slice_size)
+  array_length = input_array.size
+  second_array = [0]
+
+  (0...array_length).each do |index|
+    second_array[index + 1] = second_array[index] + input_array[index]
+  end
+
+  (0..array_length - slice_size).map do |index|
+    second_array[index + slice_size] - second_array[index]
+  end.max
 end
+
+# Alternate, slower method (t +69%)
+
+# def solution(input_array, slice_size)
+#   input_array.each_cons(slice_size).max_by(&:sum).sum
+# end
+
+# Alternate, even slower method (t +72%)
+
+# def solution(input_array, slice_size)
+#   current_sum = running_maximum = 0
+
+#   input_array.each_cons(slice_size) do |slice|
+#     current_sum = slice.sum
+#     running_maximum = current_sum if current_sum > running_maximum
+#   end
+
+#   running_maximum
+# end
+
+# Alternate, even slower method (t +179%)
+
+# def solution(input_array, slice_size)
+#   running_maximum = current_sum = 0
+#   slice_size = slice_size - 1
+
+#   input_array[...-1].each_with_index do |element, index|
+#     current_sum = element + input_array[index + 1, slice_size].sum
+#     running_maximum = current_sum if current_sum > running_maximum
+#   end
+
+#   running_maximum
+# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -2,20 +2,43 @@
 
 require 'solution'
 
+FILE_NAME = 'test-'
+FILE_INPUT1 = 'inputArray'
+FILE_INPUT2 = 'k'
+FILE_INPUT3 = ''
+FILE_INPUT4 = ''
+
 TEST_I_O = [
-  { test: 1, input: 'cabca', output: 3 }, # 01
-  { test: 2, input: 'aba', output: 2 }, # 02
-  { test: 3, input: 'ccccccccccc', output: 1 }, # 03
-  { test: 4, input: 'bcaba', output: 3 }, # 04
-  { test: 5, input: 'codesignal', output: 10 } # 05
+  { test: 1, import: false, input1: [2, 3, 5, 1, 6], input2: 2, output: 8 }, # 01
+  { test: 2, import: false, input1: [2, 4, 10, 1], input2: 2, output: 14 }, # 02
+  { test: 3, import: false, input1: [1, 3, 2, 4], input2: 3, output: 9 }, # 03
+  { test: 4, import: false, input1: [3, 2, 1, 1], input2: 1, output: 3 }, # 04
+  { test: 5, import: false, input1: [1, 3, 4, 2, 4, 2, 4], input2: 4, output: 13 }, # 05
+  { test: 6, import: 'json', input1: nil, input2: nil, output: nil }, # 06
+  { test: 7, import: false, input1: [963, 741, 22, 851, 399, 382, 190, 247, 494, 452, 891, 532, 795, 920, 465, 831, 344, 391, 582, 897, 763, 951, 735, 806, 320, 702, 200, 59, 870, 345, 695, 321, 817, 83, 416, 36, 914, 335, 117, 985, 690, 303, 875, 556, 292, 309, 496, 791, 509, 360, 235, 784, 607, 341], input2: 23, output: 14_232 }, # 07
+  { test: 8, import: 'json', input1: nil, input2: nil, output: nil }, # 08
+  { test: 9, import: 'json', input1: nil, input2: nil, output: nil }, # 09
+  { test: 10, import: 'json', input1: nil, input2: nil, output: nil } # 10
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 1 input and 1 output' do
+  it 'passes the supplied tests with 2 inputs and 1 output' do
+    start_time = Time.new
+
     TEST_I_O.each do |element|
+      case element[:import]
+      when 'json'
+        file = JSON.parse(open("docs/#{FILE_NAME}#{element[:test]}.json").read)
+        element[:input1] = file['input'][FILE_INPUT1]
+        element[:input2] = file['input'][FILE_INPUT2]
+        element[:output] = file['output']
+      end
+
       puts "  Executing test #{element[:test]}..."
-      expect(solution(element[:input])).to eq(element[:output])
+      expect(solution(element[:input1], element[:input2])).to eq(element[:output])
       puts '    Passed.'
     end
+
+    puts "Processing Time: #{((Time.now - start_time).to_f * 1000).round(5)} milliseconds."
   end
 end
