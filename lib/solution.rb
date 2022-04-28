753a38cbfdbc515d03e2339844ba0323294ece9c
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-28 12:50:20 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of longestDigitsPrefix.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
 
-def solution(value1, weight1, value2, weight2, max_weight)
-  if weight1 > max_weight && weight2 > max_weight
-    0
-  elsif weight1 + weight2 <= max_weight
-    value1 + value2
-  elsif weight1 <= max_weight && weight2 > max_weight
-    value1
-  elsif weight2 <= max_weight && weight1 > max_weight
-    value2
-  else
-    [value1, value2].max
-  end
+def solution(input_string)
+  input_string[/^\d*/]
 end
-
-# Alternate solution
-
-# def solution(value1, weight1, value2, weight2, max_weight)
-#   return 0 if weight1 > max_weight && weight2 > max_weight
-#   return value1 + value2 if weight1 + weight2 <= max_weight
-#   return value1 if weight1 <= max_weight && weight2 > max_weight
-#   return value2 if weight1 > max_weight && weight2 <= max_weight
-
-#   [value1, value2].max
-# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,22 +3,19 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input1: 10, input2: 5, input3: 6, input4: 4, input5: 8, output: 10 }, # 01
-  { test: 2, input1: 10, input2: 5, input3: 6, input4: 4, input5: 9, output: 16 }, # 02
-  { test: 3, input1: 5, input2: 3, input3: 7, input4: 4, input5: 6, output: 7 }, # 03
-  { test: 4, input1: 10, input2: 2, input3: 11, input4: 3, input5: 1, output: 0 }, # 04
-  { test: 5, input1: 15, input2: 2, input3: 20, input4: 3, input5: 2, output: 15 }, # 05
-  { test: 6, input1: 2, input2: 5, input3: 3, input4: 4, input5: 5, output: 3 }, # 06
-  { test: 7, input1: 4, input2: 3, input3: 3, input4: 4, input5: 4, output: 4 }, # 07
-  { test: 8, input1: 3, input2: 5, input3: 3, input4: 8, input5: 10, output: 3 } # 08
+  { test: 1, input: '123aa1', output: '123' }, # 01
+  { test: 2, input: '0123456789', output: '0123456789' }, # 02
+  { test: 3, input: '  3) always check for whitespaces', output: '' }, # 03
+  { test: 4, input: '12abc34', output: '12' }, # 04
+  { test: 5, input: 'the output is 42', output: '' }, # 05
+  { test: 6, input: 'aaaaaaa', output: '' } # 06
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 5 inputs and 1 output' do
+  it 'passes the supplied tests with 1 input and 1 output' do
     TEST_I_O.each do |element|
       puts "  Executing test #{element[:test]}..."
-      expect(solution(element[:input1], element[:input2], element[:input3], element[:input4],
-                      element[:input5])).to eq(element[:output])
+      expect(solution(element[:input])).to eq(element[:output])
       puts '    Passed.'
     end
   end
