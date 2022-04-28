d15d4d0b7d9e887b7ae24f645e6e5a85fa9eb249
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-28 14:39:07 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of Bishop and Pawn.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
-def solution(input_number)
-  input_number <= 9 ? 0 : 1 + solution(input_number.digits.reduce(:+))
+def solution(bishop, pawn)
+  (bishop[0].ord - pawn[0].ord).abs == (bishop[1].to_i - pawn[1].to_i).abs
 end
 
-# Alternative solution
+# Alternate solution with chessboard array
 
-# def solution(input_number)
-#   iterations = 0
+# def solution(bishop, pawn)
+#   chessboard = 8.times.map { 8.times.map { false } }
+#   bishop = [bishop[1].to_i - 1, bishop.ord - 97]
+#   pawn = [pawn[1].to_i - 1, pawn.ord - 97]
 
-#   until input_number <= 9
-#     input_number = input_number.digits.sum
-#     iterations += 1
+#   (0..7).each do |x_axis|
+#     (0..7).each do |y_axis|
+#       chessboard[x_axis][y_axis] = true if (x_axis - bishop[0]).abs == (y_axis - bishop[1]).abs
+#     end
 #   end
 
-#   iterations
+#   chessboard[pawn[0]][pawn[1]]
 # end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,23 +3,23 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input: 5, output: 0 }, # 01
-  { test: 2, input: 100, output: 1 }, # 02
-  { test: 3, input: 91, output: 2 }, # 03
-  { test: 4, input: 99, output: 2 }, # 04
-  { test: 5, input: 1_000_000_000, output: 1 }, # 05
-  { test: 6, input: 9, output: 0 }, # 06
-  { test: 7, input: 73, output: 2 }, # 07
-  { test: 8, input: 877, output: 2 }, # 08
-  { test: 9, input: 777_773, output: 3 }, # 09
-  { test: 10, input: 304, output: 1 } # 10
+  { test: 1, input1: 'a1', input2: 'c3', output: true }, # 01
+  { test: 2, input1: 'h1', input2: 'h3', output: false }, # 02
+  { test: 3, input1: 'a5', input2: 'c3', output: true }, # 03
+  { test: 4, input1: 'g1', input2: 'f3', output: false }, # 04
+  { test: 5, input1: 'e7', input2: 'd6', output: true }, # 05
+  { test: 6, input1: 'e7', input2: 'a3', output: true }, # 06
+  { test: 7, input1: 'e3', input2: 'a7', output: true }, # 07
+  { test: 8, input1: 'a1', input2: 'h8', output: true }, # 08
+  { test: 9, input1: 'a1', input2: 'h7', output: false }, # 09
+  { test: 10, input1: 'h1', input2: 'a8', output: true } # 10
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 1 input and 1 output' do
+  it 'passes the supplied tests with 2 inputs and 1 output' do
     TEST_I_O.each do |element|
       puts "  Executing test #{element[:test]}..."
-      expect(solution(element[:input])).to eq(element[:output])
+      expect(solution(element[:input1], element[:input2])).to eq(element[:output])
       puts '    Passed.'
     end
   end
