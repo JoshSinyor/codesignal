d314bbb20fb593c502fa2a1ab4a3f313492e9d99
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-04-28 13:15:31 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of digitDegree.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
-def solution(input_string)
-  input_string[/^\d*/]
+def solution(input_number)
+  input_number <= 9 ? 0 : 1 + solution(input_number.digits.reduce(:+))
 end
+
+# Alternative solution
+
+# def solution(input_number)
+#   iterations = 0
+
+#   until input_number <= 9
+#     input_number = input_number.digits.sum
+#     iterations += 1
+#   end
+
+#   iterations
+# end
--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,12 +3,16 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, input: '123aa1', output: '123' }, # 01
-  { test: 2, input: '0123456789', output: '0123456789' }, # 02
-  { test: 3, input: '  3) always check for whitespaces', output: '' }, # 03
-  { test: 4, input: '12abc34', output: '12' }, # 04
-  { test: 5, input: 'the output is 42', output: '' }, # 05
-  { test: 6, input: 'aaaaaaa', output: '' } # 06
+  { test: 1, input: 5, output: 0 }, # 01
+  { test: 2, input: 100, output: 1 }, # 02
+  { test: 3, input: 91, output: 2 }, # 03
+  { test: 4, input: 99, output: 2 }, # 04
+  { test: 5, input: 1_000_000_000, output: 1 }, # 05
+  { test: 6, input: 9, output: 0 }, # 06
+  { test: 7, input: 73, output: 2 }, # 07
+  { test: 8, input: 877, output: 2 }, # 08
+  { test: 9, input: 777_773, output: 3 }, # 09
+  { test: 10, input: 304, output: 1 } # 10
 ].freeze
 
 describe 'solution' do
