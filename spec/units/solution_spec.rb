0d008e5b6a9dd45aa7fb18ce371c71e6f88ccc61
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2023-03-16 14:43:04 +0000

    Update solution.rb and solution_spec.rb. Reflects completion of Is MAC48 Address?

--- spec/units/solution_spec.rb
+++ spec/units/solution_spec.rb
@@ -3,27 +3,30 @@
 require 'solution'
 
 TEST_I_O = [
-  { test: 1, import: false, input1: [2, 3, 5, 2], input2: 3, output: 2 }, # 01
-  { test: 2, import: false, input1: [1, 3, 3, 1, 1], input2: 0, output: 0 }, # 02
-  { test: 3, import: false, input1: [5, 1, 3, 4, 1], input2: 0, output: 1 }, # 03
-  { test: 4, import: false, input1: [1, 1, 1, 1], input2: 1, output: 4 }, # 04
-  { test: 5, import: false, input1: [1, 1, 1, 1], input2: 0, output: 0 }, # 05
-  { test: 6, import: false, input1: [3, 1, 1, 3, 1], input2: 2, output: 2 } # 06
+  { test: 1, import: false, input: '00-1B-63-84-45-E6', output: true }, # 01
+  { test: 2, import: false, input: 'Z1-1B-63-84-45-E6', output: false }, # 02
+  { test: 3, import: false, input: 'not a MAC-48 address', output: false }, # 03
+  { test: 4, import: false, input: 'FF-FF-FF-FF-FF-FF', output: true }, # 04
+  { test: 5, import: false, input: '00-00-00-00-00-00', output: true }, # 05
+  { test: 6, import: false, input: 'G0-00-00-00-00-00', output: false }, # 06
+  { test: 7, import: false, input: '02-03-04-05-06-07-', output: false }, # 07
+  { test: 8, import: false, input: '12-34-56-78-9A-BC', output: true }, # 08
+  { test: 9, import: false, input: 'FF-FF-AB-CD-EA-BC', output: true }, # 09
+  { test: 10, import: false, input: '12-34-56-78-9A-BG', output: false } # 10
 ].freeze
 
 describe 'solution' do
-  it 'passes the supplied tests with 2 inputs and 1 output' do
+  it 'passes the supplied tests with 1 input and 1 output' do
     TEST_I_O.each do |element|
       case element[:import]
       when 'json'
         file = JSON.parse(open("docs/#{FILE_NAME}#{element[:test]}.json").read)
-        element[:input1] = file['input'][FILE_INPUT1]
-        element[:input2] = file['input'][FILE_INPUT2]
+        element[:input1] = file['input'][FILE_INPUT]
         element[:output] = file['output']
       end
 
       puts "  Executing test #{element[:test]}..."
-      expect(solution(element[:input1], element[:input2])).to eq(element[:output])
+      expect(solution(element[:input])).to eq(element[:output])
       puts '    Passed.'
     end
   end
