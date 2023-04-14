ec899370af42d8403f0af40e486378b480d8d8e6
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2023-04-14 12:54:33 +0100

    Update solution.rb and solution_spec.rb. Reflects completion of spiralNumbers.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,18 +1,49 @@
 # frozen_string_literal: true
 
-# You are taking part in an Escape Room challenge designed specifically for programmers. In your efforts to find a clue,
-# you've found a binary code written on the wall behind a vase, and realized that it must be an encrypted message. After
-# some thought, your first guess is that each consecutive 8 bits of the code stand for the character with the
-# corresponding extended ASCII code.
+# Construct a square matrix with a size N × N containing integers from 1 to N * N in a spiral order, starting from
+# top-left and in clockwise direction.
 
-# Assuming that your hunch is correct, decode the message.
-
-def solution(*binary)
-  binary.pack('B*')
+def solution(num)
+  num = num**2 + 1
+  arr = []
+  arr = [arr.map { num -= 1 }] + arr.transpose.reverse while num > 1
+  arr.map!(&:reverse)
 end
 
-# Alternate solution using summation of bits in base-2 (binary).
+# Alternate solution using x and y coordinates.
 
-# def solution(binary)
-#   binary.chars.each_slice(8).map { |slice| slice.join.to_i(2).chr }.join
+# def solution(num)
+#   integers = (1..num**2).to_a.reverse
+#   matrix = Array.new(num).map { Array.new(num, []) }
+
+#   xrng = (0...num)
+#   yrng = (0...num)
+#   x = 0
+#   y = 0
+
+#   until integers.empty?
+#     if x == xrng.min && y == yrng.min
+#       xrng.each { |icol| matrix[y][icol] = [integers.pop] }
+#       x = xrng.max
+#       y += 1
+#       yrng = y..yrng.max
+#     elsif x == xrng.max && y == yrng.min
+#       yrng.each { |irow| matrix[irow][x] = [integers.pop] }
+#       x -= 1
+#       y = yrng.max
+#       xrng = xrng.min..x
+#     elsif x == xrng.max && y == yrng.max
+#       xrng.reverse_each { |icol| matrix[y][icol] = integers.pop }
+#       x = xrng.min
+#       y -= 1
+#       yrng = yrng.min..y
+#     elsif x == xrng.min && y == yrng.max
+#       yrng.reverse_each { |irow| matrix[irow][x] = integers.pop }
+#       x += 1
+#       y = yrng.min
+#       xrng = x..xrng.max
+#     end
+#   end
+
+#   matrix.map(&:flatten)
 # end
